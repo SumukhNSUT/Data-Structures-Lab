7f275c46a96427c8d7afff727b58f2ceb3eb07c4
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-03-26 16:58:11 +0530

    dsa lab file

---
+++ tableOfContents.cpp
@@ -0,0 +1,49 @@
+// 1. Write a program to find the mean and the median of the numbers stored in an array.
+
+// 2. Write a program to insert one element in an array and delete an element from an array.
+
+// 3. Write a program to search for a number in an array.
+
+// 4. Write a program to sort an array.
+
+// 5. Write a program to merge two sorted arrays.
+
+// 6. Write a program to store the marks obtained by 10 students in 5 courses in a two-dimensional array.
+
+// 7. Write a program to implement a linked list.
+
+// 8. Write a program to insert a node in a linked list and delete a node from a linked list.
+
+// 9. Write a program to print the elements of a linked list in reverse order without disturbing the linked list.
+
+// 10. Write a program to reverse a linked list.
+
+// 11. Write a program to add two polynomials using linked lists.
+
+// 12. Write a program to implement a doubly-linked list.
+
+// 13. Write a program to implement a stack using an array.
+
+// 14. Write a program to implement a stack using a linked list.
+
+// 15. Write a program to implement a queue using an array.
+
+// 16. Implement a queue using a linked list.
+
+// 17. Implement a circular queue using arrays.
+
+// 18. Implement a priority queue using a linked list.
+
+// 19. Implement a double-ended queue using a linked list.
+
+// 20. Construct a binary tree and display its preorder, inorder and postorder traversals.
+
+// 21. Construct a binary search tree.
+
+// 22. Construct a graph.
+
+// 23. Calculate the distance between 2 vertices in a graph.
+
+// 24. Calculate the distances between every pair of vertices in a graph.
+
+// 25. Construct a minimal spanning tree of a graph.
